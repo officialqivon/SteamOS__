fc7fadb6a8f7005238d77143699d9c1d6a249181
officialqivon <official.qivongmail.com>
2026-03-11 21:00:58 +0300

    Install plymouth-plugin-script

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ouex pipefail
 
+### Install Plymouth script plugin
+dnf5 install -y plymouth-plugin-script
+
 ### Install Plymouth theme
 cp -r /ctx/plymouth/steamos-deck /usr/share/plymouth/themes/
 plymouth-set-default-theme steamos-deck
