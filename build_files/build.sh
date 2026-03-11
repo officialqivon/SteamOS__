1151c7e5201de4c32d8a04abde7ae018f4fffe04
officialqivon <official.qivongmail.com>
2026-03-12 00:06:50 +0300

    Replace Bazzite startup video with real Steam Deck video

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ouex pipefail
 
-### Install Plymouth theme
-cp -r /ctx/plymouth/steamos-deck /usr/share/plymouth/themes/
-plymouth-set-default-theme steamos-deck
+### Replace Bazzite branding videos with real Steam Deck videos
+cp /ctx/videos/deck_startup.webm /usr/share/ublue-os/bazzite/bazzite.webm
+cp /ctx/videos/deck_startup.webm /usr/share/ublue-os/bazzite/bazzite-oled.webm
 
 ### Enable System Units
 systemctl enable podman.socket
